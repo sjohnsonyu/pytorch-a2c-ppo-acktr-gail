fb7f8790f38e74f837b268d177aa9edd3722e8aa
Sonja Johnson-Yu <sonjyu@stanford.edu>
2024-03-31 16:25:05 -0400

    Add angle_wall plus hunger as allowed observation mode

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -171,7 +171,7 @@ def get_args():
     parser.add_argument("--move_cost", type=float, default=DEFAULT_MOVE_COST, help="Agent move cost")
     parser.add_argument("--turn_cost", type=float, default=DEFAULT_TURN_COST, help="Agent turn cost")
     parser.add_argument("--num_food", type=int, default=10, help="Number of food in the arena")
-    parser.add_argument("--observation_mode", type=str, default="vector", choices=["distance", "vector", "angle_dist", "angle_wall"], help="Observation mode for agent")
+    parser.add_argument("--observation_mode", type=str, default="vector", choices=["distance", "vector", "angle_dist", "angle_wall", "angle_wall_hunger"], help="Observation mode for agent")
     parser.add_argument("--motion_mode", type=str, default="simple", choices=["simple", "kinetic"], help="Motion mode for agent")
     parser.add_argument("--reward_mode", type=str, default="food_distance_shaping", choices=["food_distance_shaping", "food_eaten", "hunger_meter"], help="Reward mode for agent")
     parser.add_argument("--food_init_pos_mode", type=str, default="random_with_close", choices=["random", "random_with_close"], help="Food position mode")
